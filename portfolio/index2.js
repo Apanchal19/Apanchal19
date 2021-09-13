d53f68de1b7741a318b7165d6072f33f689dd798
Akshay Panchal <akshay.panchalhp@gmail.com>
2021-09-13 19:57:48 +0530

    My website

---
+++ portfolio/index2.js
@@ -0,0 +1,24 @@
+var $filters = $('.filter [data-filter]'),
+    $boxes = $('.boxes [data-category]');
+
+$filters.on('click', function(e) {
+  e.preventDefault();
+  var $this = $(this);
+  $filters.removeClass('active');
+  $this.addClass('active');
+
+  var $filterColor = $this.attr('data-filter');
+
+  if ($filterColor == 'all') {
+    $boxes.removeClass('is-animated')
+      .fadeOut().promise().done(function() {
+        $boxes.addClass('is-animated').fadeIn();
+      });
+  } else {
+    $boxes.removeClass('is-animated')
+      .fadeOut().promise().done(function() {
+        $boxes.filter('[data-category = "' + $filterColor + '"]')
+          .addClass('is-animated').fadeIn();
+      });
+  }
+});
